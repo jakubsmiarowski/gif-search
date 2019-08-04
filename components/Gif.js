4a1aa1cb1826aefc4c460a94ecfd1be4c5c5af79
Jakub Smiarowski <jakub.smiarowski@gmail.com>
2019-08-04 12:56:51 +0200

    Add ES6

--- components/Gif.js
+++ components/Gif.js
@@ -20,3 +20,31 @@ Gif = React.createClass({
     );
   }
 });
+
+//ES6
+
+const GIPHY_LOADING_URL = 'http://www.ifmo.ru/images/loader.gif';
+const styles = {
+  minHeight: '310px',
+  margin: '0.5em'
+};
+
+class Gif extends React.Component{
+
+  getUrl() {
+    return this.props.sourceUrl || GIPHY_LOADING_URL;
+  }
+
+  render() {
+    let url = this.props.loading ? GIPHY_LOADING_URL : this.props.url;
+
+    return (
+      <div style={styles}>
+        <a href={this.getUrl()} title='view this on giphy' target='new'>
+          <img id='gif' src={url} style={{width: '100%', maxWidth: '350px'}}/>
+        </a>
+      </div>
+    );
+  }
+
+}
--- components/Search.js
+++ components/Search.js
@@ -35,3 +35,42 @@ Search = React.createClass({
             />
   }
 });
+
+//ES6
+class Search extends React.Component{
+  
+  getInitialState() {
+    return {
+        searchingText: ''
+    };
+  }
+
+  handleChange(event){
+    let searchingText = event.target.value;
+    this.setState({searchingText: searchingText});
+
+    if (searchingText.length > 2) {
+      this.props.onSearch(searchingText);
+    }
+  }
+
+  handleKeyUp(event){
+    if (event.keyCode === 13) {
+      this.props.onSearch(this.state.searchingText);
+    }
+  }
+
+  render() {
+    const styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
+
+    return <input
+             type="text"
+             onChange={this.handleChange}
+             onKeyUp={this.handleKeyUp}
+             placeholder="Tutaj wpisz wyszukiwaną frazę"
+             style={styles}
+             value={this.state.searchTerm}
+            />
+  }
+
+}
