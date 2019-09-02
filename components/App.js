4ecd19689dba1b0185f2b2bbc5f9842c2be1a3c9
Jakub Smiarowski <jakub.smiarowski@gmail.com>
2019-09-02 21:15:53 +0200

    fixed missing api

--- components/App.js
+++ components/App.js
@@ -25,8 +25,13 @@ App = React.createClass({
 
 
  getGif: function(searchingText){
+
+    var GIPHY_API_URL = 'https://api.giphy.com';
+    var GIPHY_PUB_KEY = 'zzAPElO6vYjAgtoTfzsDzR9iDsRswjjW';
+
      return new Promise(
         (resolve,reject) => {
+            var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
             var xhr = new XMLHttpRequest();
             xhr.open('GET', url);
             xhr.onload = function() {
